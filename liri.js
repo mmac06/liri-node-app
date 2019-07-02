78f58c85d04083e61c199d558a0a3b76184363d2
Mike McMahon <mikemcmahon@isen-macbook02.local>
2019-07-01 23:33:40 -0500

    added concert display

--- liri.js
+++ liri.js
@@ -18,37 +18,73 @@ var saysText = "";
 var userTrigger = process.argv[2];
 var nodeArgs = process.argv;
 
-// var bandQuery = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
 
 // if user types "movie-this" it taps into spofity API and same for the other two user inputs
 if (userTrigger === "movie-this") {
     for (var i = 3; i < nodeArgs.length; i++) {
-        movieName += nodeArgs[i]
+        if (i > 2 && i < nodeArgs.length) {
+            movieName = movieName + "+" + nodeArgs[i];
+        } else movieName += nodeArgs[i]
+
     }
 }
 else if (userTrigger === "concert-this") {
     for (var i = 3; i < nodeArgs.length; i++) {
-        concertName += nodeArgs[i]
+        if (i > 3 && i < nodeArgs.length) {
+            concertName = concertName + "+" + nodeArgs[i];
+        } else concertName += nodeArgs[i];
+        axios.get("https://rest.bandsintown.com/artists/" + concertName + "/events?app_id=codingbootcamp").then(
+            function (response) {
+                // Returns message if no matches found
+                if (response.data.length === 0) {
+                    console.log("No records found.");
+                }
+                else {
+                    for (var i = 0; i < response.data.length; i++) {
+                        var event = response.data[i];
+                        var concertDate = moment(event.datetime).format("MM/DD/YYYY")
+                        //    Displays concert venue, city, and date in MM/DD/YYYY format
+                        console.log("Concert venue:", event.venue.name);
+                        console.log("Concert city:", event.venue.city);
+                        console.log("Concert date:", concertDate + "\n");
+                    }
+                }
+            }
+        )
+
+
     }
 } else if (userTrigger === "spotify-this") {
     for (var i = 3; i < nodeArgs.length; i++) {
-        spotifyName += nodeArgs[i]
+        if (i > 3 && i < nodeArgs.length) {
+            spotifyName = concertName + "+" + nodeArgs[i];
+            spotifyName += nodeArgs[i]
+        }
     }
-} 
-// else if (userTrigger === "do-what-it-says") {
-//     // FIGURE OUT LATER XXX
+    // else if (userTrigger === "do-what-it-says") {
+    //     // FIGURE OUT LATER XXX
 
-// }
+}
 else {
     console.log("LIRI doesn't have this information.")
 }
-var movieQuery = "http://www.omdbapi.com/?t=" + movieName + "i=tt3896198&apikey=dd77419d";
-var spofityQuery = ;
-var concertQuery ;
+
+var concertQuery = "https://rest.bandsintown.com/artists/" + concertName + "/events?app_id=codingbootcamp";
+var movieQuery = "http://www.omdbapi.com/?t=" + movieName + "&i=tt3896198&apikey=dd77419d";
+// var spotifyQuery = spotify.search({ type: 'track', query: spotifyName }, function (err, data) {
+//     if (err) {
+//         return console.log('Spotify error occurred: ' + err);
+//     }
+
+//     console.log("spotData:", data);
+// });
 
 
+
+
+console.log("concertQuery:", concertQuery);
 console.log("movieQuery:", movieQuery);
+// console.log("spotifyQuery:", spotifyQuery);
 
 // concert-this
 // spotify-this-song
