8d3fc2f131cb62410ce534ac4176253a61485221
Mike McMahon <mikemcmahon@isen-macbook02.local>
2019-07-01 21:29:37 -0500

    added var pick

--- liri.js
+++ liri.js
@@ -3,8 +3,30 @@ require("dotenv").config();
 var Spotify = require("node-spotify-api");
 
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 
 var axios = require("axios");
-var moment = require ("moment");
+var moment = require("moment");
 var fs = require("fs");
+
+var spotify = new Spotify(keys.spotify);
+
+
+// SEE 7:10 IN CLASS VIDEO
+var pick = function (caseData, functionData) {
+    switch caseData {
+        case "concert-this":
+            getBands(functionData);
+            break;
+        case "spotify-this-song":
+            getSpotify(functionData);
+            break;
+        case "movie-this":
+            getMovie(functionData);
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+            default:
+                console.log("LIRI doesn't have this information.")
+    }
+};
