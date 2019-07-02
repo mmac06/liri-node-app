96a853ba93892b1f67c68adda4e3430a293f912f
Mike McMahon <mikemcmahon@isen-macbook02.local>
2019-07-01 20:48:50 -0500

    imported vars

--- liri.js
+++ liri.js
@@ -1,4 +1,10 @@
 require("dotenv").config();
 
+var Spotify = require("node-spotify-api");
+
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+var axios = require("axios");
+var moment = require ("moment");
+var fs = require("fs");
