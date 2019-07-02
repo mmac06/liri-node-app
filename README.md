9cd9ed3cd5b1216dcadc9639a2cf079b9e8d4173
Mike McMahon <mikemcmahon@isen-macbook02.local>
2019-07-01 22:11:50 -0500

    movie search function working

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # liri-node-app
+
+Create and use a standard GitHub repository. As this is a CLI App, it cannot be deployed to GitHub pages or Heroku. This time you'll need to include screenshots, a GIF, and/or a video showing us that you have the app working with no bugs. You can include these screenshots/GIFs or a link to a video in a README.md file.
+
+In order to meet the Employer Competitive standards and be ready to show your application to employers, the README.md file should meet the following criteria:
+
+
+Clearly state the problem the app is trying to solve (i.e. what is it doing and why)
+Give a high-level overview of how the app is organized
+Give start-to-finish instructions on how to run the app
+Include screenshots, gifs or videos of the app functioning
+Contain a link to a deployed version of the app
+Clearly list the technologies used in the app
+State your role in the app development
+
+
+Because screenshots (and well-written READMEs) are extremely important in the context of GitHub, this will be part of the grading in this assignment.
+
--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 var Spotify = require("node-spotify-api");
 
-var keys = require("./keys.js");
+var keys = require("./keys");
 
 var axios = require("axios");
 var moment = require("moment");
@@ -10,10 +10,53 @@ var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 
+var bandName = "";
+var movieName = "";
+var songName = "";
+
+var userTrigger = process.argv[2];
+var nodeArgs = process.argv;
+
+// var bandQuery = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+
+// if user types "concert-this" it taps into spofity API and same for the other two user inputs
+if (userTrigger==="movie-this"){
+    for (var i = 3; i < nodeArgs.length; i++) {
+
+        movieName += nodeArgs[i]
+
+
+        // if (i > 2 && i < nodeArgs.length) {
+        //     movieName = movieName + "+" + nodeArgs[i];
+        // } else {
+        //     movieName += nodeArgs[i];
+        // }
+    }
+
+
+}
+var movieQuery = "http://www.omdbapi.com/?t=" + movieName + "i=tt3896198&apikey=dd77419d";
+console.log("movieQuery:", movieQuery);
+
+// concert-this
+// spotify-this-song
+// movie-this
+// do-what-it-says
+
+
+
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+
+
+
+
+// -------------------------
 
 // SEE 7:10 IN CLASS VIDEO
 var pick = function (caseData, functionData) {
-    switch caseData {
+    switch (caseData) {
         case "concert-this":
             getBands(functionData);
             break;
@@ -26,7 +69,7 @@ var pick = function (caseData, functionData) {
         case "do-what-it-says":
             doWhatItSays();
             break;
-            default:
-                console.log("LIRI doesn't have this information.")
+        default:
+            console.log("LIRI doesn't have this information.")
     }
 };
